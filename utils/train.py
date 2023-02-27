e65a6b91c724ab8788714eb85b38290f3dc35c3f
ljy <hyleepp@gmail.com>
2023-02-27 10:47:13 +0800

    refactoring

--- utils/train.py
+++ utils/train.py
@@ -1,5 +1,8 @@
 import datetime 
 import os
+import json
+from collections import defaultdict
+import logging
 
 DOTS = '********'
 
@@ -66,3 +69,99 @@ def tensor2set(tensor) -> set:
         ret.add((tensor[i][0].item(), tensor[i][1].item(), tensor[i][2].item()))
     
     return ret
+
+def load_classes(path):
+    """load the idx2class dict (1: 'human', 2: "film")
+    the class information is get from the "is instance of" relation from wikidata. And we will
+    use this class information to filter some ridiculous pairs of entity and relation (like [human, isLocated])
+
+    ps:
+    key means sth like /m/012qdp
+    qid means id in wiki, like Q5
+    idx means id in triples, int number, i-th entity
+
+    Args:
+        path (_type_): the path of data 
+
+    Returns:
+        idx2class: a dict that map each idx to its class like {1: 'human'}
+        class_names: the values of idx2class
+    """
+    key2qid, key2idx, idx2class = [{} for _ in range(3)]
+
+    with open(os.path.join(path, 'entity2wikidata.json'), 'r') as f:
+        entity2wiki = json.load(f)
+        for key, value in entity2wiki.items():
+            key2qid[key] = value['wikidata_id']
+
+    with open(os.path.join(path, 'entity2id.txt'), 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            key, idx = line.strip().split()
+            key2idx[key] = int(idx)
+
+    with open(os.path.join(path, "entity_type.json"), 'r') as f:
+        qid2class = json.load(f)
+
+    for k, v in entity2wiki.items():
+        wiki_id = v['wikidata_id']
+        class_name = qid2class[wiki_id]['parent_entity_name']
+        idx = key2idx[k]
+        idx2class[idx] = class_name
+    
+    return idx2class 
+
+def generate_relation_filter(triples: list, id2class: dict, n_rel: int, rec=False) -> dict:
+    """generate the relation filter based on entity class (like 'human').
+    That contains the legal pattern appeared in triples, like ['human', 'like'],
+    and helps to filter out the ridiculous combination like ['human', 'isLocated']
+
+    Args:
+        triples (list): [(h,r,t)] 
+        id2class (dict): {1: 'human'} 
+        n_rel (int): how many rel, since we may add rec
+        rec (bool): add reciprocal relations or not
+
+    Returns:
+        dict: like {human: [like, isWife, plays...]} 
+    """
+    # generate the relation filter based on entity class (like human).
+
+    class_filter_relation = defaultdict(set)
+    for h, r, t in triples:
+        if h in id2class.keys() and t in id2class.keys():
+            # there may have some entity miss the corresponding descriptions
+            h_class, t_class = id2class[h], id2class[t]
+            class_filter_relation[h_class].add(r)
+            if rec:
+                class_filter_relation[t_class].add(r + n_rel)
+    
+    return class_filter_relation
+    
+
+def show_the_cover_rate(unexplored_triples:list, init_filter:dict, idx2class:dict, n_rel:int) -> None:
+    """show the cover rate of init filter on unexplored triples
+
+    Args:
+        unexplored_triples (list): [h, r, t]
+        init_filter (dict): {class: [r1, r2....]}
+        idx2class (dict): {1: 'human'}
+        n_rel (int): the number of relations 
+    """
+
+    count_in_init = 0
+    count_class_not_recorded = 0 # some entity miss the wiki information
+
+    for triple in unexplored_triples:
+        h, r, t = triple
+        h_class, t_class = idx2class.get(h), idx2class.get(t)
+        if h_class and t_class:
+            if r in init_filter[h_class] or r + n_rel in init_filter[t_class]:
+                count_in_init += 1
+        else:
+            count_class_not_recorded += 1 
+
+    logging.info(f'init filter cover rate: {count_in_init / len(unexplored_triples)}')
+    logging.info(f'init filter + ignore unrecorded cover rate: {(count_in_init + count_class_not_recorded) / len(unexplored_triples)}')
+
+    return
