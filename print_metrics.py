67bedb8ff7f0f343ada7fb66dfb79158c876095d
lrlbbzl <1766718768@qq.com>
2023-03-12 07:20:42 +0000

    fix print_metric

--- print_metrics.py
+++ print_metrics.py
@@ -5,9 +5,15 @@ from scipy import integrate
 
 from numpy import trapz
 
-def get_metrics(path):
+def get_metrics(path, boundary):
     # path = 'confirmed_logs/QuatE_No_reg/events.out.tfevents.1678184630.autodl-container-f77d11adac-015069fd.2076.0'
-
+    """
+    Args:
+        path: event file path
+        boundary: default 0.99 
+    Returns:
+        step, area, max_completion
+    """
     ea=event_accumulator.EventAccumulator(path) 
     ea.Reload()
     # print(ea.scalars.Keys())
@@ -19,13 +25,8 @@ def get_metrics(path):
     x = [i.step for i in val_psnr]
     y = [i.value for i in val_psnr]
 
+    # step, -1 if model cannot meet expectation
+    step = -1 if max(y) < boundary else max(x)
+    # The area enclosed by the completion rate curve and the left boundary
     area = (max(x) - min(x)) * max(y) - integrate.simps(y, x, dx=0.0001)
-    # print((max(x) - min(x)) * max(y) - integrate.trapz(y, x, dx=0.0001))
-    # plt.xlabel('x axis')
-    # plt.ylabel('y axis')
-    # plt.legend(loc=4) # 指定legend的位置,读者可以自己help它的用法
-    # plt.title('polyfitting')
-    # plt.savefig('images/savefig_example.png')
-    # plt.show()
-
-    return area
+    return step, area, max(y)
